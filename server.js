92107c75d420e9f8f0bd9aa8f4a398ba819359a2
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-05-11 17:29:55 +0530

    Auth Update

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 import express from "express"
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 import cors from "cors"
 import morgan from 'morgan';
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import connectDB from './config/db.js';
 import userRoutes from "./routes/userRoutes.js"
 import propertyRoutes from "./routes/propertyRoutes.js"
 
 dotenv.config()
 
+connectDB();
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
+// app.use(cors({
+//   origin: [
+//     process.env.CLIENT_URL,
+//     'http://localhost:5173',
+//     'https://propertynearme-client.vercel.app'
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
+
+
 app.use(cors({
   origin: [
     process.env.CLIENT_URL,
-    'http://localhost:5173',
-    'https://propertynearme-client.vercel.app'
+    'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -41,16 +55,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB")
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
-  })
-  .catch((error) => {
-    console.error("MongoDB connection error:", error)
-  })
 
+// const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
